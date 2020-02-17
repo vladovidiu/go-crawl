7afc46155fc27f1072c496aba6c594d3ceb4a474
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2020-02-17 15:13:10 +0000

    Redo logic

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/steelx/extractlinks v0.0.2
+	github.com/steelx/webscrapper v0.0.0-20191217122227-455793d2152a
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
--- main.go
+++ main.go
@@ -6,99 +6,121 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
 
 	"github.com/steelx/extractlinks"
+	"github.com/steelx/webscrapper/graph"
 )
 
 var (
-	config = &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	transport = &http.Transport{
-		TLSClientConfig: config,
-	}
-	netClient = &http.Client{
-		Transport: transport,
-	}
-	queue      = make(chan string)
-	hasVisited = make(map[string]bool)
+	urlQueue   = make(chan string)
+	config     = &tls.Config{InsecureSkipVerify: true}
+	transport  = &http.Transport{TLSClientConfig: config}
+	hasCrawled = make(map[string]bool)
+	netClient  *http.Client
+	graphMap   = graph.NewGraph()
 )
 
-func main() {
-	arguments := os.Args[1:]
+func init() {
+	netClient = &http.Client{Transport: transport}
+	go SignalHandler(make(chan os.Signal, 1))
+}
 
-	if len(arguments) == 0 {
-		fmt.Println("Missing URL")
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("URL is missing")
 		os.Exit(1)
 	}
 
-	baseURL := arguments[0]
+	baseUrl := args[0]
 
 	go func() {
-		queue <- baseURL
+		urlQueue <- baseUrl
 	}()
 
-	for href := range queue {
-		if !hasVisited[href] && isSameDomain(href, baseURL) {
-			crawlUrl(href)
+	for href := range urlQueue {
+		if !hasCrawled[href] && isSameDomain(href, baseUrl) {
+			crawlLink(href)
 		}
 	}
 }
 
-func crawlUrl(href string) {
-	hasVisited[href] = true
-	fmt.Printf("Crawling url -> %v \n", href)
-	response, err := netClient.Get(href)
-	defer response.Body.Close()
+func crawlLink(baseHref string) {
+	graphMap.AddVertex(baseHref)
+	hasCrawled[baseHref] = true
+	fmt.Println("Crawling... ", baseHref)
+	resp, err := netClient.Get(baseHref)
 	checkErr(err)
+	defer resp.Body.Close()
 
-	links, err := extractlinks.All(response.Body)
+	links, err := extractlinks.All(resp.Body)
 	checkErr(err)
 
 	for _, link := range links {
-		go func() {
-			queue <- toFixedUrl(link.Href, href)
-		}()
+		if link.Href == "" {
+			continue
+		}
+		fixedUrl := toFixedUrl(link.Href, baseHref)
+		if baseHref != fixedUrl {
+			graphMap.AddEdge(baseHref, fixedUrl)
+		}
+		go func(url string) {
+			urlQueue <- url
+		}(fixedUrl)
 	}
 }
 
-func toFixedUrl(href, baseUrl string) string {
+func toFixedUrl(href, base string) string {
 	uri, err := url.Parse(href)
+	if err != nil || uri.Scheme == "mailto" || uri.Scheme == "tel" {
+		return base
+	}
+	baseUrl, err := url.Parse(base)
 	if err != nil {
 		return ""
 	}
-
-	base, err := url.Parse(baseUrl)
-	if err != nil {
-		return ""
-	}
-
-	toFixedUri := base.ResolveReference(uri)
-
-	return toFixedUri.String()
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
 }
 
-func isSameDomain(href, baseUrl string) bool {
+func isSameDomain(href, base string) bool {
 	uri, err := url.Parse(href)
 	if err != nil {
 		return false
 	}
-
-	parentUri, err := url.Parse(baseUrl)
+	parentUrl, err := url.Parse(base)
 	if err != nil {
 		return false
 	}
-
-	if uri.Host != parentUri.Host {
+	if uri.Host != parentUrl.Host {
 		return false
 	}
-
 	return true
 }
 
 func checkErr(err error) {
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(0)
+	}
+}
+
+func SignalHandler(c chan os.Signal) {
+	signal.Notify(c, os.Interrupt)
+
+	for s := <-c; ; s = <-c {
+		switch s {
+		case os.Interrupt:
+			fmt.Println("^C received")
+			fmt.Println("<----------- ----------- ----------- ----------->")
+			fmt.Println("<----------- ----------- ----------- ----------->")
+			graphMap.CreatePath("https://www.monzo.co.uk", "https://app.adjust.com/help")
+			os.Exit(0)
+		case os.Kill:
+			fmt.Println("SIGKILL received")
+			os.Exit(1)
+		}
 	}
 }
